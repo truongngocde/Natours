49b51b834eaf9c2ef774a93ba1127d1691f5a74f
truongngocde <detruong2002@gmail.com>
2023-10-27 16:17:53 +0700

    Get Request

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
+//     res.status(200).json({
+//         title: 'Learning NodeJs',
+//         content: "Study with me",
+
+//     });
+// })
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'));
+app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
-        title: 'Learning NodeJs',
-        content: "Study with me",
-        author: "Truong Ngoc De"
+        status:'success',
+        data: {
+            tours
+        }
     });
-})
+});
 
 const port = 8000;
 app.listen(port, () => {
