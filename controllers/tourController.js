28920268467e1180d1bd05bb5771176cdfec73d5
truongngocde <detruong2002@gmail.com>
2023-11-02 17:20:29 +0700

    Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,34 @@
+const { query } = require('express');
 const Tour = require('./../models/tourModel');
 
 // GET -> Read
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    // 1. Filtering
+    const queryObj = {...req.query};
+    const excludedFieds = ['page', 'sort', 'limit', 'fields'];
+    excludedFieds.forEach(param => delete queryObj[param]);
+
+    // 2. Advenced Filtering
+    console.log(queryObj);
+    // {difficulty: 'easy', duration: {gte: 5}}
+    // {difficulty: 'easy', duration: {$gte: 5}}
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    console.log(JSON.parse(queryStr));
+    
+    
+
+    const query = Tour.find(JSON.parse(queryStr));
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+    // EXECUTE QUERY    
+    
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
@@ -16,7 +41,7 @@ exports.getAllTours = async (req, res) => {
     res.status(404).json({
       status: 'fail',
       message: error,
-    })
+    });
   }
 };
 
@@ -34,7 +59,7 @@ exports.getTour = async (req, res) => {
     res.status(404).json({
       status: 'fail',
       message: error,
-    })
+    });
   }
 };
 
@@ -59,25 +84,24 @@ exports.createTour = async (req, res) => {
 // PATCH -> Update
 exports.updateTour = async (req, res) => {
   try {
-    // new = true -> khi update trả lại giá trị đã update 
+    // new = true -> khi update trả lại giá trị đã update
     // runValidators: true -> chạy lại ràng buộc dữ liệu
     const updateTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
-      runValidators: true
-    })
+      runValidators: true,
+    });
     res.status(200).json({
       status: 'Success',
       data: {
-        updateTour
+        updateTour,
       },
     });
   } catch (error) {
     res.status(404).json({
       status: 'fail',
       message: error,
-    })
+    });
   }
-  
 };
 
 // DELETE -> Delete
@@ -94,6 +118,6 @@ exports.deleteTour = async (req, res) => {
     res.status(404).json({
       status: 'fail',
       message: error,
-    })
+    });
   }
 };
