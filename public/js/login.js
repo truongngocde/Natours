8619755542d1dade91ef427755d408812df4d8a8
truongngocde <detruong2002@gmail.com>
2023-11-19 23:35:04 +0700

    finalLogin(alert)

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,7 @@
-const login = async (email, password) => {
+import axios from "axios";
+import { showAlert } from "./alert";
+
+export const login = async (email, password) => {
   console.log(email, password);
   try {
     const res = await axios({
@@ -10,19 +13,13 @@ const login = async (email, password) => {
       },
     });
     if (res.data.status === 'success') {
-      alert('Logged in successfully !');
+      showAlert('success','Logged in successfully !');
       window.setTimeout(() => {
         location.assign('/');
       }, 1500);
     }
   } catch (error) {
-    alert(error.response.data.message);
+    showAlert('error',error.response.data.message);
   }
 };
 
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
