b9e278f3f4c20d1b7ba155c67a0164e034425c80
truongngocde <detruong2002@gmail.com>
2023-12-02 18:06:34 +0700

    vercel

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12379,7 +12379,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54490" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64077" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
